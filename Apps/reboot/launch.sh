f712f0916e633b9685e7cb909ec515fba5c724af
Ninoh-FOX <ninohfox@gmail.com>
2024-12-22 14:40:28 +0100

    Actualizar launch.sh

--- Apps/reboot/launch.sh
+++ Apps/reboot/launch.sh
@@ -1,34 +1,5 @@
 #!/bin/sh
 
-# Function to terminate all non-essential processes
-task_killer() {
-    for pid in /proc/[0-9]*; do
-        pid=$(basename "$pid")
-        
-        # Ignore critical processes and the script's own process
-        if [ "$pid" != "1" ] && [ "$pid" != "$$" ]; then
-            kill $1 "$pid" 2>/dev/null
-        fi
-    done
-}
-
-# Attempt graceful shutdown of processes
-kill_all_processes() {
-    echo "Attempting to gracefully shut down processes..."
-    task_killer -TERM
-    sleep 0.5  # Wait to allow processes to terminate gracefully
-
-    # Forcefully kill any remaining processes
-    echo "Forcing termination of remaining processes..."
-    task_killer -KILL
-}
-
-# Close all possible processes before rebooting
-kill_all_processes
-
-# Synchronize data with storage to minimize data corruption risks
-sync
-
-# Reboot the system
-echo "Rebooting the system..."
-reboot
+cp -f /mnt/SDCARD/Apps/reboot/reboot /tmp/reboot
+set -m
+/tmp/reboot
